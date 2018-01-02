50fcad51b0c1068152bfb5d00ee65c3f9d15f4f3
ersabha <sabhadec11@gmail.com>
2018-01-02 22:17:43 +0530

    searching and sorting added

--- main.js
+++ main.js
@@ -1,3 +1,56 @@
+ var songs = [{
+                    'name': 'Tera Zikr',
+                    'artist': 'Darshan Raval',
+                    'album': 'DR-Album',
+                    'duration': '4:08',
+                    'fileName': 'song1.mp3',
+                    'image': 'song1.jpg'
+                },
+
+                {
+                    'name': 'Teri Meri Dosti',
+                    'artist': 'Darshan Raval',
+                    'album': 'DR-Album',
+                    'duration': '2:54',
+                    'fileName': 'song2.mp3',
+                    'image': 'song2.jpg'
+                },
+                {
+                    'name': 'Tu Dua Hai',
+                    'artist': 'Darshan Raval',
+                    'album': 'DR-Album',
+                    'duration': '3:27',
+                    'fileName': 'song3.mp3',
+                    'image': 'song3.jpg'
+                },
+                {
+                    'name': 'Mere Nishaan',
+                    'artist': 'Darshan Raval',
+                    'album': 'DR-Album',
+                    'duration': '4:10',
+                    'fileName': 'song4.mp3',
+                    'image': 'song4.jpg'
+                },
+
+                {
+                    'name': 'Jeena Jeena',
+                    'artist': 'Darshan Raval',
+                    'album': 'DR-Album',
+                    'duration': '3:02',
+                    'fileName': 'song5.mp3',
+                    'image': 'song5.jpg'
+                },
+
+                {
+                    'name': 'Saari ki Saari',
+                    'artist': 'Darshan Raval',
+                    'album': 'DR-Album',
+                    'duration': '2:59',
+                    'fileName': 'song6.mp3',
+                    'image': 'song6.jpg'
+                }]
+
+
     function fancyTimeFormat(time)
     {   
         // Hours, minutes and seconds
@@ -33,6 +86,17 @@
         }
         } 
 
+
+
+
+        function changeCurrentSongDetails(songObj) {
+                    $('.current-song-image').attr('src','img/' + songObj.image)
+                    $('.current-song-name').text(songObj.name)
+                    $('.current-song-album').text(songObj.album)
+        }
+
+
+
         function updateCurrentTime() {
             var song = document.querySelector('audio');
             var currentTime = Math.floor(song.currentTime);
@@ -45,59 +109,50 @@
 
 
 
-        function addSongNameClickEvent(songName,position) {
+        function addSongNameClickEvent(songObj,position) {
+            var songName = songObj.fileName;
             var id = '#song' + position;
                 $(id).click(function() {
-                var audio = document.querySelector('audio');
-                var currentSong = audio.src;
-                if(currentSong.search(songName) != -1)
-                    {
-                    toggleSong();
+                    var audio = document.querySelector('audio');
+                    var currentSong = audio.src;
+                    if(currentSong.search(songName) != -1)
+                        {
+                        toggleSong();
+                        }
+                    else {
+                        audio.src = songName;
+                        toggleSong();
+                        changeCurrentSongDetails(songObj);
                     }
-                else {
-                    audio.src = songName;
-                    toggleSong();
-                }
                 });
-                }
-
+                }           
      
 
         window.onload = function() {
-          
-            updateCurrentTime(); 
-            setInterval(function() {
-            updateCurrentTime();
-            },1000);
 
-            var songList = ['Tera Zikr','Teri Meri Dosti', 'Tu Dua Hai', 'Mere Nishaan', 'Jeena Jeena', 'Saari ki Saari' ]; 
-      
+                   $('#songs').DataTable({
+                     paging: false
+                     });
 
-            var artistList = ['Darshan Raval','Darshan Raval','Darshan Raval','Darshan Raval','Darshan Raval','Darshan Raval' ];
-             
-            var albumList = ['DR-Album','DR-Album','DR-Album','DR-Album','DR-Album','DR-Album' ]; 
+                   changeCurrentSongDetails(songs[0]);
+                    updateCurrentTime(); 
+                    setInterval(function() {
+                    updateCurrentTime();
+                    },1000);
+ 
 
-            var durationList = ['4:08','2:54','3:27','4:10','3:02','2:59' ]; 
-
-
-                 for(var i =0; i < songList.length;i++) {
+                 for(var i =0; i <= songs.length;i++) {
+                        var obj = songs[i];
                         var name = '#song' + (i+1);
                         var song = $(name);
-                        song.find('.song-name').text(songList[i]);
-                        song.find('.song-artist').text(artistList[i]);
-                        song.find('.song-album').text(albumList[i]); // Added
-                        song.find('.song-length').text(durationList[i]); // Added
+                        song.find('.song-name').text(obj.name);
+                        song.find('.song-artist').text(obj.artist);
+                        song.find('.song-album').text(obj.album);
+                        song.find('.song-length').text(obj.duration);
+                        addSongNameClickEvent(obj,i+1);
                     }
-
-
-
-            var fileNames = ['song1.mp3','song2.mp3','song3.mp3','song4.mp3','song5.mp3', 'song6.mp3'];   
-                
-            for (var i = 0; i < fileNames.length ; i++) {
-                addSongNameClickEvent(fileNames[i],i+1)
-            } 
-
-            }
+                      
+               }
         
 
     $('.welcome-screen button').on('click', function() {
